80cbbf97aa4d8177d3cf4d9ad75510553a8b8bdf
Shilo B <shiloworks@gmail.com>
2017-08-28 18:12:05 +1000

    Added rollup config.

---
+++ assets/angular/manageRoles/rollup-config.js
@@ -0,0 +1,24 @@
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs    from 'rollup-plugin-commonjs';
+
+export default {
+  entry: './main.js',
+  dest: './dist-bundle.js', // output a single application bundle
+  sourceMap: false,
+  format: 'iife',
+  onwarn: function(warning) {
+    // Skip certain warnings
+
+    // should intercept ... but doesn't in some rollup versions
+    if ( warning.code === 'THIS_IS_UNDEFINED' ) { return; }
+
+    // console.warn everything else
+    console.warn( warning.message );
+  },
+  plugins: [
+      nodeResolve({jsnext: true, module: true}),
+      commonjs({
+        include: '../../../node_modules/**'
+      })
+  ]
+}
