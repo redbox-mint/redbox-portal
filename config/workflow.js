d9ed990775955d3c016e689e4cab369927150e1e
Shilo B <shiloworks@gmail.com>
2017-08-29 14:09:39 +1000

    Removed active tab/workflow, fixed text.

--- config/workflow.js
+++ config/workflow.js
@@ -5,7 +5,6 @@ module.exports.workflow = {
         workflow: {
           stage: 'draft',
           stageLabel: 'Draft',
-          next: 'active',
         },
         authorization: {
           viewRoles: ['Admin','Librarians'],
@@ -14,20 +13,5 @@ module.exports.workflow = {
         form: 'default-1.0-draft'
       },
       starting: true
-    },
-    "active": {
-      config: {
-        workflow: {
-          stage: 'active',
-          stageLabel: 'Active',
-          back: 'draft'
-        },
-        authorization: {
-          viewRoles: ['Admin','Librarians'],
-          editRoles: ['Admin','Librarians']
-        },
-        form: 'default-1.0-active'
-      }
     }
-  }
 };
