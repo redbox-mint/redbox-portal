74e74bee9de35a86838cdc2c39bbf99672814b01
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2017-12-13 13:09:44 +1030

    Removed obsolete email param from login command

--- dockerhub_deploy.sh
+++ dockerhub_deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 export REPO=qcifengineering/redbox-portal
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH; fi`
 docker build -f Dockerfile -t $REPO:$COMMIT .
