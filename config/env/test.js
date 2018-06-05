9165c69816bc1997818ccdb0522da48cef13f5a3
Shilo B <shiloworks@gmail.com>
2018-06-05 16:02:54 +1000

    Updated datastores config in test.

--- config/env/test.js
+++ config/env/test.js
@@ -51,5 +51,11 @@ module.exports = {
   },
   redbox: {
     apiKey: '18f344b4-81a9-43b9-8916-4bbf339e8765'
+  },
+  datastores:{
+    mongodb: {
+      adapter: require('sails-mongo'),
+      url: 'mongodb://mongodb:27017/redbox-portal'
+    }
   }
 };
