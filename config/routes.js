0b30ae9ade58cfad5f435ee39781008f5fd6f152
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2018-08-14 13:20:09 +0930

    Fixed partial function to support running in production mode Added External Service option to vocab component

--- config/routes.js
+++ config/routes.js
@@ -184,6 +184,7 @@ module.exports.routes = {
   'get /:branding/:portal/vocab/:vocabId': 'VocabController.get',
   'get /:branding/:portal/ands/vocab/resourceDetails': 'VocabController.rvaGetResourceDetails',
   'get /:branding/:portal/mint/:mintSourceType': 'VocabController.getMint',
+  'post /:branding/:portal/external/vocab/:provider': 'VocabController.searchExternalService',
   'get /:branding/:portal/collection/:collectionId': 'VocabController.getCollection',
   'post /:branding/:portal/collection/:collectionId': 'VocabController.loadCollection',
   'get /:branding/:portal/export': 'ExportController.index',
