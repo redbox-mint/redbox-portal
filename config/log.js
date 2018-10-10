e37448bbccbd54cb7a8147d1db88ce8fc7f574d0
Mike Lynch <mike@mikelynch.org>
2018-10-10 11:38:22 +1100

    Cleaned up some bits of the GitLab branch which got checked in here by mistake

--- config/log.js
+++ config/log.js
@@ -10,35 +10,6 @@
  * http://sailsjs.org/#!/documentation/concepts/Logging
  */
 
-var winston = require('winston');
-
-/*see the documentation for Winston:  https://github.com/flatiron/winston */
-var logger = new(winston.Logger)({
-  transports: [
-    new (winston.transports.Console)({}),
-    new (winston.transports.File)({
-      filename: 'logs/verbose.log',
-      level: 'info',
-      json: false,
-      colorize: false
-    })
-  ]
-});
-
 module.exports.log = {
-  /***************************************************************************
-   *                                                                          *
-   * Valid `level` configs: i.e. the minimum log level to capture with        *
-   * sails.log.*()                                                            *
-   *                                                                          *
-   * The order of precedence for log levels from lowest to highest is:        *
-   * silly, verbose, info, debug, warn, error                                 *
-   *                                                                          *
-   * You may also set the level to "silent" to suppress all logs.             *
-   *                                                                          *
-   ***************************************************************************/
 
-  level: 'info',
-  colorize: false,
-  custom: logger
 };
--- config/workspacetype.js
+++ config/workspacetype.js
@@ -2,12 +2,5 @@
 
 
 module.exports.workspacetype = {
-    'gitlab': {
-        name: 'gitlab',
-        label: 'GitLab',
-        subtitle: 'GitLab',
-        description: 'Create or link GitLab workspace:',
-        logo: '/angular/gitlab/assets/images/gitlab.png'
-    }
 } 
 
