09eb3de17afa0a71a542ba9f557dcb7f949272bb
Shilo B <shiloworks@gmail.com>
2019-06-12 14:33:10 +1000

    Removed unused test. Added logs during bootstrap.

--- test/unit/services/FormsService.test.js
+++ test/unit/services/FormsService.test.js
@@ -7,7 +7,7 @@ describe('The FormsService', function () {
     var brand = BrandingService.getDefault();
     var recordType = 'rdmp';
     var formName = 'default-1.0-draft';
-    RecordType.find().then(forms => {sails.log.error(`going to look for ${brand.id}_${formName}`);sails.log.error(forms);});
+    RecordType.find().then(forms => {sails.log.verbose(`going to look for ${brand.id}_${formName}`);sails.log.verbose(forms);});
 
     FormsService.getForm( brand.id,recordType, true).subscribe(function(form) {
       expect(form).to.have.property('name', formName);
