03d922a997a7978d18340529d985b7a9b6211186
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2019-07-04 14:37:29 +0930

    Fixed syntax error

--- typescript/api/services/UsersService.ts
+++ typescript/api/services/UsersService.ts
@@ -182,7 +182,7 @@ export module Services {
 
     protected openIdConnectAuth = () => {
       const defAuthConfig = ConfigService.getBrand(BrandingService.getDefault().name, 'auth');
-      if (defAuthConfig.active.instanceOf('oidc') != -1) {
+      if (defAuthConfig.active.indexOf('oidc') != -1) {
         const oidcOpts = defAuthConfig.oidc.opts;
         let OidcStrategy = require('passport-openidconnect').Strategy;
         sails.config.passport.use('oidc', new OidcStrategy(oidcOpts.oidcStrategyOptions, (req, issuer, sub, profile, accessToken, refreshToken, done) => {
