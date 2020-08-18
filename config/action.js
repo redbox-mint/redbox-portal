b3978b5f9eb9b23f58c8be07854e220bf7fd4fae
Shilo B <shiloworks@gmail.com>
2020-08-18 10:16:52 +1000

    Improvement: removed eval in ActionController, allowed services to respond directly to clients.

--- config/action.js
+++ config/action.js
@@ -1,3 +1,5 @@
 module.exports.action = {
-  publishToCKAN: "sails.services.ckanservice.publishToCKAN"
+  // Here you can configure your own custom actions, for hooks, etc.
+  // Follow the sample convention below:
+  // publishToCKAN: {service: "sails.services.ckanservice", method: "publishToCKAN" }
 }
--- typescript/api/controllers/ActionController.ts
+++ typescript/api/controllers/ActionController.ts
@@ -20,6 +20,7 @@
 //<reference path='./../../typings/loader.d.ts'/>
 declare var module;
 declare var sails;
+declare var _;
 import { Observable } from 'rxjs/Rx';
 declare function require(name:string);
 
@@ -50,13 +51,17 @@ export module Controllers {
     public callService(req, res) {
       const actionName = req.param('action')
       const oid = req.param('oid');
-      const options = {};
-      let serviceFunction = eval(sails.config.action[actionName]);
-      let response = serviceFunction(req.body, options);
-
-      return response.subscribe( result => {
-        return this.ajaxOk(req, res, null, result);
-      });
+      const config = sails.config.action[actionName];
+      const options = {config: config};
+      let serviceFunction = _.get(config.service, config.method);
+      // Can optionally return an observable to subscribe on if this is a lengthy and complicated call
+      // For simpler operations, service functions can write directly to the response object
+      const response = serviceFunction(req, res, options);
+      if (!res.writableEnded) {
+        return response.subscribe( result => {
+          return this.ajaxOk(req, res, null, result);
+        });
+      }
     }
 
 
