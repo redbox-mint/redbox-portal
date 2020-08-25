9362ba5dde3ed84ebf6ae5712bfc1c228e396bae
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2020-08-25 23:16:20 +0930

    Updated docker deploy script to support bcryptjs fork

--- dockerhub_deploy.sh
+++ dockerhub_deploy.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 export REPO=qcifengineering/redbox-portal
-export TAG=`if [ "$CIRCLE_BRANCH" == "master" ]; then echo "latest"; else echo $CIRCLE_BRANCH; fi`
+export TAG=`if [ "$CIRCLE_BRANCH" == "master" && "$1" == "" ]; then echo "latest"; else echo $CIRCLE_BRANCH; fi`
+
+#if parameter is passed to script, add parameter to tag. Used to produce bcryptjs versions of the app
+export TAG=`if [ "$1" != "" ]; then echo "$TAG-$1"; else echo $TAG; fi`
 docker build -f Dockerfile -t $REPO:$CIRCLE_SHA1 .
 docker tag $REPO:$CIRCLE_SHA1 $REPO:$TAG
 docker tag $REPO:$CIRCLE_SHA1 $REPO:circleci-$CIRCLE_BUILD_NUM
