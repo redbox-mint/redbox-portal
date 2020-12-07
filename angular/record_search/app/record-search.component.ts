9bad4e817a88fadc6f669f6c4f98a5e1601a2a71
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2020-12-08 10:28:29 +1030

    Removed sails.io from the js bundle. To be reinstated when we use websockets Filtered out workspaces from search options

--- angular/record_search/app/record-search.component.ts
+++ angular/record_search/app/record-search.component.ts
@@ -17,16 +17,42 @@
 // with this program; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-import { Component, Inject, Input, Output, ElementRef, EventEmitter } from '@angular/core';
-import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
-import { Role, User, LoginResult, SaveResult } from './shared/user-models';
+import {
+  Component,
+  Inject,
+  Input,
+  Output,
+  ElementRef,
+  EventEmitter
+} from '@angular/core';
+import {
+  Location,
+  LocationStrategy,
+  PathLocationStrategy
+} from '@angular/common';
+import {
+  Role,
+  User,
+  LoginResult,
+  SaveResult
+} from './shared/user-models';
 import * as _ from "lodash";
-import { LoadableComponent } from './shared/loadable.component';
-import { TranslationService } from './shared/translation-service';
-import { RecordsService, RecordSearchParams, RecordSearchRefiner} from './shared/form/records.service';
-import { DashboardService } from './shared/dashboard-service';
+import {
+  LoadableComponent
+} from './shared/loadable.component';
+import {
+  TranslationService
+} from './shared/translation-service';
+import {
+  RecordsService,
+  RecordSearchParams,
+  RecordSearchRefiner
+} from './shared/form/records.service';
+import {
+  DashboardService
+} from './shared/dashboard-service';
 
-declare var pageData :any;
+declare var pageData: any;
 declare var jQuery: any;
 /**
  * Record Search component
@@ -38,7 +64,10 @@ declare var jQuery: any;
   moduleId: module.id,
   selector: 'record-search',
   templateUrl: './record_search.html',
-  providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}]
+  providers: [Location, {
+    provide: LocationStrategy,
+    useClass: PathLocationStrategy
+  }]
 })
 export class RecordSearchComponent extends LoadableComponent {
   @Input() record_type: string;
@@ -57,11 +86,11 @@ export class RecordSearchComponent extends LoadableComponent {
   recTypeNames: string[];
 
   constructor(
-   elm: ElementRef,
-   @Inject(Location) protected LocationService: Location,
-   protected recordsService: RecordsService,
-   protected dashboardService: DashboardService,
-   public translationService:TranslationService) {
+    elm: ElementRef,
+    @Inject(Location) protected LocationService: Location,
+    protected recordsService: RecordsService,
+    protected dashboardService: DashboardService,
+    public translationService: TranslationService) {
     super();
     this.initTranslator(translationService);
     this.record_type = elm.nativeElement.getAttribute('record_type');
@@ -72,39 +101,40 @@ export class RecordSearchComponent extends LoadableComponent {
   }
 
   ngOnInit() {
-    this.translationService.isReady((tService:any)=> {
+    this.translationService.isReady((tService: any) => {
       this.waitForInit([
         this.recordsService
-        ], () => {
-      this.recordsService.getAllTypes().then((typeConfs: any) => {
-        _.each(typeConfs, typeConf => {
-          this.recTypeNames.push(typeConf.name);
-          const searchParam = new RecordSearchParams(typeConf.name);
-          const searchFilterConfig = [];
-          _.forEach(typeConf.searchFilters, (searchConfig:any)=> {
-            searchFilterConfig.push(new RecordSearchRefiner(searchConfig));
+      ], () => {
+        this.recordsService.getAllTypes().then((typeConfs: any) => {
+          _.each(typeConfs, typeConf => {
+            if (typeConf.oackageType != 'workspace') {
+              this.recTypeNames.push(typeConf.name);
+              const searchParam = new RecordSearchParams(typeConf.name);
+              const searchFilterConfig = [];
+              _.forEach(typeConf.searchFilters, (searchConfig: any) => {
+                searchFilterConfig.push(new RecordSearchRefiner(searchConfig));
+              });
+              searchParam.setRefinerConfig(searchFilterConfig);
+              this.paramMap[typeConf.name] = searchParam;
+            }
           });
-          searchParam.setRefinerConfig(searchFilterConfig);
-          this.paramMap[typeConf.name] = searchParam;
-        });
-        this.setRecordType(this.record_type);
-        this.setLoading(false);
-        if (!_.isEmpty(this.queryStr)) {
-          console.log(`Using query string: ${this.queryStr}`);
-          this.params.parseQueryStr(this.queryStr);
-          this.search(null, false);
-        }
-        this.LocationService.subscribe((popState:any) => {
-          this.queryStr = popState.url.split('?')[1];
-          this.params.parseQueryStr(this.queryStr);
-          this.search(null, false);
-        }, (exception: any) => {
-          console.log(`Error on location service monitoring.`);
-          console.log(exception);
-        }, ()=> {
+          this.setRecordType(this.record_type);
+          this.setLoading(false);
+          if (!_.isEmpty(this.queryStr)) {
+            console.log(`Using query string: ${this.queryStr}`);
+            this.params.parseQueryStr(this.queryStr);
+            this.search(null, false);
+          }
+          this.LocationService.subscribe((popState: any) => {
+            this.queryStr = popState.url.split('?')[1];
+            this.params.parseQueryStr(this.queryStr);
+            this.search(null, false);
+          }, (exception: any) => {
+            console.log(`Error on location service monitoring.`);
+            console.log(exception);
+          }, () => {});
         });
       });
-    });
     });
   }
 
@@ -144,7 +174,7 @@ export class RecordSearchComponent extends LoadableComponent {
     }
   }
 
-  search(refinerConfig: RecordSearchRefiner = null, shouldSyncLoc:boolean = true) {
+  search(refinerConfig: RecordSearchRefiner = null, shouldSyncLoc: boolean = true) {
     if (!_.isEmpty(this.params.basicSearch)) {
       if (refinerConfig) {
         this.params.addActiveRefiner(refinerConfig);
@@ -156,14 +186,14 @@ export class RecordSearchComponent extends LoadableComponent {
       if (shouldSyncLoc) {
         this.syncLoc();
       }
-      this.recordsService.search(this.params).then((res:any) => {
+      this.recordsService.search(this.params).then((res: any) => {
         this.isSearching = false;
         this.searchMsgType = "success";
         this.searchMsg = `${this.translationService.t('record-search-results')}${res.records.length}`;
         this.dashboardService.setDashboardTitle(null, res.records);
         this.params.setFacetValues(res.facets);
         this.plans = res.records;
-      }).catch((err:any) => {
+      }).catch((err: any) => {
         this.isSearching = false;
         this.searchMsg = err;
         this.searchMsgType = "danger";
--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -26,7 +26,7 @@ var cssFilesToInject = [
 var jsFilesToInject = [
 
   // Load sails.io before everything else
-  'js/dependencies/sails.io.js',
+  // 'js/dependencies/sails.io.js',
 
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
