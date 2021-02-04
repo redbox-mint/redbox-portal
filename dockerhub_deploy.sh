779b7e6ce50ae426c2249ee202e1e91a78e151a2
Shilo Banihit <shiloworks@gmail.com>
2021-02-04 14:04:08 +1000

    Reverted dockerhub build tag support.

--- dockerhub_deploy.sh
+++ dockerhub_deploy.sh
@@ -2,9 +2,6 @@
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 export REPO=qcifengineering/redbox-portal
 export TAG=`if [ "$CIRCLE_BRANCH" == "master" ] && [ -z "$1" ]; then echo "latest"; else echo "$CIRCLE_BRANCH"; fi`
-if [ -z "$TAG" ]; then
-  export TAG=`echo "$CIRCLE_TAG"`
-fi
 #if parameter is passed to script, add parameter to tag. Used to produce bcryptjs versions of the app
 export TAG=`if [ "$1" != "" ]; then echo "$TAG-$1"; else echo "$TAG"; fi`
 docker build -f Dockerfile -t $REPO:$CIRCLE_SHA1 .
