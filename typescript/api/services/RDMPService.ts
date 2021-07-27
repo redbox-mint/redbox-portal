8ed149bb5882bea39f04bcf1af227b6f20464f11
Andrew Brazzatti <a.brazzatti@qcif.edu.au>
2021-07-27 14:10:20 +0000

    Added implementation to support queueing trigger functions.

--- typescript/api/services/RDMPService.ts
+++ typescript/api/services/RDMPService.ts
@@ -17,11 +17,20 @@
 // with this program; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-import { Observable } from 'rxjs/Rx';
+import {
+  Observable
+} from 'rxjs/Rx';
 import services = require('../core/CoreService.js');
-import { Sails, Model } from "sails";
+import {
+  Sails,
+  Model
+} from "sails";
 import moment = require('moment');
 import * as numeral from 'numeral';
+import QueueService from '../core/QueueService.js';
+import {
+  isObservable
+} from 'rxjs';
 
 declare var sails: Sails;
 declare var RecordType, Counter: Model;
@@ -39,6 +48,8 @@ export module Services {
    */
   export class RDMPS extends services.Services.Core.Service {
 
+    protected queueService: QueueService = null;
+
     protected _exportedMethods: any = [
       'assignPermissions',
       'complexAssignPermissions',
@@ -48,6 +59,15 @@ export module Services {
       'runTemplates',
       'addWorkspaceToRecord'
     ];
+
+    constructor() {
+      super();
+      this.logHeader = "TriggerService::";
+      let that = this;
+      sails.on('ready', function () {
+        that.queueService = sails.services[sails.config.queue.serviceName];
+      });
+    }
 
     /**
      * This is a trigger service method to bump all configured increment counters.
@@ -79,7 +99,14 @@ export module Services {
       // get the counters
       _.each(options.counters, (counter: any) => {
         if (counter.strategy == "global") {
-          obs.push(this.getObservable(Counter.findOrCreate({ name: counter.field_name, branding: brandId }, { name: counter.field_name, branding: brandId, value: 0 })));
+          obs.push(this.getObservable(Counter.findOrCreate({
+            name: counter.field_name,
+            branding: brandId
+          }, {
+            name: counter.field_name,
+            branding: brandId,
+            value: 0
+          })));
         } else if (counter.strategy == "field") {
           let srcVal = record.metadata[counter.field_name];
           if (!_.isEmpty(counter.source_field)) {
@@ -99,7 +126,11 @@ export module Services {
               let counter = options.counters[idx];
               let newVal = counterVal[0].value + 1;
               this.incrementCounter(record, counter, newVal);
-              updateObs.push(this.getObservable(Counter.updateOne({ id: counterVal[0].id }, { value: newVal })));
+              updateObs.push(this.getObservable(Counter.updateOne({
+                id: counterVal[0].id
+              }, {
+                value: newVal
+              })));
             });
             return Observable.zip(...updateObs);
           })
@@ -111,8 +142,14 @@ export module Services {
 
     private incrementCounter(record: any, counter: any, newVal: any) {
       if (!_.isEmpty(counter.template)) {
-        const imports = _.extend({ moment: moment, numeral: numeral, newVal: newVal }, counter);
-        const templateData = { imports: imports };
+        const imports = _.extend({
+          moment: moment,
+          numeral: numeral,
+          newVal: newVal
+        }, counter);
+        const templateData = {
+          imports: imports
+        };
         const template = _.template(counter.template, templateData);
         newVal = template();
       }
@@ -159,7 +196,7 @@ export module Services {
     }
 
     protected getContribListByRule(contribProperties, record, rule, emailProperty, emailList) {
-      let compiledRule = _.template(rule); 
+      let compiledRule = _.template(rule);
       _.each(contribProperties, contributorProperty => {
         sails.log.verbose(`Processing contributor property ${contributorProperty}`)
         let contributor = _.get(record, contributorProperty, null);
@@ -168,12 +205,12 @@ export module Services {
           sails.log.verbose(JSON.stringify(contributor));
           if (_.isArray(contributor)) {
             _.each(contributor, individualContributor => {
-              if(compiledRule(individualContributor) == true || compiledRule(individualContributor) == "true") {
+              if (compiledRule(individualContributor) == true || compiledRule(individualContributor) == "true") {
                 this.addEmailToList(individualContributor, emailProperty, emailList);
               }
             });
           } else {
-            if(compiledRule(contributor) == true || compiledRule(contributor) == "true") {
+            if (compiledRule(contributor) == true || compiledRule(contributor) == "true") {
               this.addEmailToList(contributor, emailProperty, emailList);
             }
           }
@@ -193,6 +230,56 @@ export module Services {
       });
     }
 
+    public queueTriggerCall(oid, record, options, user) {
+      let jobName = _.get(options, "jobName", null);
+      let triggerConfiguration = _.get(options, "triggerConfiguration", null);
+      let queueMessage = {
+        oid: oid,
+        record: record,
+        triggerConfiguration: triggerConfiguration,
+        user: user
+      };
+      sails.log.debug(`${this.logHeader} Queueing up trigger using job name ${jobName}`);
+      sails.log.verbose(queueMessage);
+      this.queueService.now(jobName, queueMessage);
+    }
+
+    public async queuedTriggerSubscriptionHandler(job: any) {
+      let oid = _.get(job, "oid", null);
+      let triggerConfiguration = _.get(job, "triggerConfiguration", null);
+      let record = _.get(job, "record", null);
+      let user = _.get(job, "user", null);
+
+          sails.log.debug(triggerConfiguration);
+          let hookFunctionString = _.get(triggerConfiguration, "function", null);
+          if (hookFunctionString != null) {
+            let hookFunction = eval(hookFunctionString);
+            let options = _.get(triggerConfiguration, "options", {});
+            if (_.isFunction(hookFunction)) {
+              sails.log.debug(`Triggering queuedtrigger: ${hookFunctionString}`)
+              let hookResponse = hookFunction(oid, record, options, user);
+              let response = await this.resolveHookResponse(hookResponse);
+              sails.log.debug(`${hookFunctionString} response now is:`);
+              sails.log.verbose(JSON.stringify(response));
+              sails.log.debug(`queued trigger ${hookFunctionString} completed for ${oid}`)
+            } else {
+              sails.log.error(`queued trigger function: '${hookFunctionString}' did not resolve to a valid function, what I got:`);
+              sails.log.error(hookFunction);
+            }
+          }
+    
+    }
+
+    private resolveHookResponse(hookResponse) {
+      let response = hookResponse;
+      if (isObservable(hookResponse)) {
+        response = hookResponse.toPromise();
+      } else {
+        response = Promise.resolve(hookResponse);
+      }
+      return response;
+    }
+
     public complexAssignPermissions(oid, record, options) {
       sails.log.verbose(`Complex Assign Permissions executing on oid: ${oid}, using options:`);
       sails.log.verbose(JSON.stringify(options));
@@ -204,7 +291,7 @@ export module Services {
       const viewPermissionRule = _.get(options, "viewPermissionRule");
       const editPermissionRule = _.get(options, "editPermissionRule");
       const recordCreatorPermissions = _.get(options, "recordCreatorPermissions");
-      
+
       let editContributorObs = [];
       let viewContributorObs = [];
       let editContributorEmails = [];
@@ -227,10 +314,14 @@ export module Services {
         return Observable.of(record);
       }
       _.each(editContributorEmails, editorEmail => {
-        editContributorObs.push(this.getObservable(User.findOne({ email: editorEmail.toLowerCase() })));
+        editContributorObs.push(this.getObservable(User.findOne({
+          email: editorEmail.toLowerCase()
+        })));
       });
       _.each(viewContributorEmails, viewerEmail => {
-        viewContributorObs.push(this.getObservable(User.findOne({ email: viewerEmail.toLowerCase() })));
+        viewContributorObs.push(this.getObservable(User.findOne({
+          email: viewerEmail.toLowerCase()
+        })));
       });
       let zippedViewContributorUsers = null;
       if (editContributorObs.length == 0) {
@@ -296,10 +387,14 @@ export module Services {
         return Observable.of(record);
       }
       _.each(editContributorEmails, editorEmail => {
-        editContributorObs.push(this.getObservable(User.findOne({ email: editorEmail.toLowerCase() })));
+        editContributorObs.push(this.getObservable(User.findOne({
+          email: editorEmail.toLowerCase()
+        })));
       });
       _.each(viewContributorEmails, viewerEmail => {
-        viewContributorObs.push(this.getObservable(User.findOne({ email: viewerEmail.toLowerCase() })));
+        viewContributorObs.push(this.getObservable(User.findOne({
+          email: viewerEmail.toLowerCase()
+        })));
       });
       let zippedViewContributorUsers = null;
       if (editContributorObs.length == 0) {
@@ -398,8 +493,17 @@ export module Services {
       try {
         _.each(options.templates, (templateConfig) => {
           tmplConfig = templateConfig;
-          const imports = _.extend({oid: oid, record: record, user: user, options: options, moment: moment, numeral:numeral}, this);
-          const templateData = {imports: imports};
+          const imports = _.extend({
+            oid: oid,
+            record: record,
+            user: user,
+            options: options,
+            moment: moment,
+            numeral: numeral
+          }, this);
+          const templateData = {
+            imports: imports
+          };
           const data = _.template(templateConfig.template, templateData)();
           _.set(record, templateConfig.field, data);
         });
