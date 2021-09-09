06a9258decacd3e1f3ab539fc65a85ee0be822d0
Andrew Brazzatti <a.brazzatti@qcif.edu.au>
2021-09-09 23:17:10 +0000

    Added improved handling and logging for user audits

--- typescript/api/controllers/UserController.ts
+++ typescript/api/controllers/UserController.ts
@@ -116,8 +116,11 @@ export module Controllers {
       let user = req.session.user ? req.session.user : req.user;
 
       req.logout();
-      UsersService.addUserAuditEvent(user, "logout", requestDetails).subscribe(response => {
+      UsersService.addUserAuditEvent(user, "logout", requestDetails).then(response => {
         sails.log.debug(`User logout audit event created: ${user.id}`)
+      }).catch(err => {
+        sails.log.error(`User logout audit event failed`)
+        sails.log.error(err)
       });
       req.session.destroy(err => {
         res.redirect(redirUrl);
@@ -221,8 +224,11 @@ export module Controllers {
         let requestDetails = new RequestDetails(req);
         // We don't want to store the password!
         delete requestDetails.body.password;
-        UsersService.addUserAuditEvent(user, "login", requestDetails).subscribe(response => {
+        UsersService.addUserAuditEvent(user, "login", requestDetails).then(response => {
           sails.log.debug(`User login audit event created for local login: ${user.id}`)
+        }).catch(err => {
+          sails.log.error(`User login audit event created for local login failed`)
+          sails.log.error(err)
         });
         req.logIn(user, function (err) {
           if (err) res.send(err);
@@ -278,8 +284,11 @@ export module Controllers {
           }); 
         }       
         let requestDetails = new RequestDetails(req);
-        UsersService.addUserAuditEvent(user, "login", requestDetails).subscribe(response => {
+        UsersService.addUserAuditEvent(user, "login", requestDetails).then(response => {
           sails.log.debug(`User login audit event created for OIDC login: ${user.id}`)
+        }).catch(err => {
+          sails.log.error(`User login audit event created for OIDC login failed`)
+          sails.log.error(err)
         });
 
         req.logIn(user, function (err) {
@@ -319,8 +328,11 @@ export module Controllers {
         }
 
         let requestDetails = new RequestDetails(req);
-        UsersService.addUserAuditEvent(user, "login", requestDetails).subscribe(response => {
+        UsersService.addUserAuditEvent(user, "login", requestDetails).then(response => {
           sails.log.debug(`User login audit event created for AAF login: ${user.id}`)
+        }).catch(err => {
+          sails.log.error(`User login audit event created for AAF login failed`)
+          sails.log.error(err)
         });
 
         req.logIn(user, function (err) {
