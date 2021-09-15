1f1df2039c232409f139d56db2f3c4d17b138edb
Andrew Brazzatti <andrew.brazzatti@qcif.edu.au>
2021-09-15 13:09:03 +0000

    Updated SearchService interface to handle pagination

--- core/src/SearchService.ts
+++ core/src/SearchService.ts
@@ -1,7 +1,7 @@
 export interface SearchService{
 
   index(id:string, data:any):any;
-  searchFuzzy(type, workflowState, searchQuery, exactSearches, facetSearches, brand, user, roles, returnFields): Promise<any>;
+  searchFuzzy(type, workflowState, searchQuery, exactSearches, facetSearches, brand, user, roles, returnFields, start?:number, rows?:number): Promise<any>;
   remove(id: string): any;
   searchAdvanced(query): Promise<any>;
 }
