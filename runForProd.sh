75b7ee540995ba76558d39708743b9397bac2543
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2021-12-24 00:45:43 +0000

    Moved run script commands to npm scripts. Removed more bash scripts no longer used. Added new angular build script

--- runForProd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export buildTarget="PROD"
-source dev_build/buildFns.sh
-cleanUpAllJs
-docker run -it --rm -u "node" -v $PWD:/opt/rds-rdmp-portal qcifengineering/dlcf-portal:latest /bin/bash -c "cd /opt/rds-rdmp-portal; npm install;"
-linkNodeLib "lodash-es" "lodash-lib"
-compileAoT
-export ENV=production
-docker-compose up
