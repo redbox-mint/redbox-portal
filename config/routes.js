df929c17d3e8d5e4892d0fc1356dd92988e24993
Shilo Banihit <shiloworks@gmail.com>
2022-03-21 13:58:52 +1000

    - Disabled named queries route - Preparing for 2.0.5 release

--- config/routes.js
+++ config/routes.js
@@ -423,12 +423,12 @@ module.exports.routes = {
     action: 'sendNotification',
     csrf: false
   },
-  'get /:branding/:portal/api/report/namedQuery': {
-    controller: 'webservice/ReportController',
-    action: 'executeNamedQuery',
-    csrf: false
-  },
-
+  // TODO: named queries staged but not disabled, uncomment when ready
+  // 'get /:branding/:portal/api/report/namedQuery': {
+  //   controller: 'webservice/ReportController',
+  //   action: 'executeNamedQuery',
+  //   csrf: false
+  // },
   'get /:branding/:portal/workspaces/types/:name': 'WorkspaceTypesController.getOne',
   'get /:branding/:portal/workspaces/types': 'WorkspaceTypesController.get'
 };
