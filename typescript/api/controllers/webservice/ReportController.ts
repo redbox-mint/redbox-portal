f3b18961ce107fdf59070b719ab61afe5b5a923e
alejandro-bulgaris-qcif <74333434+alejandro-bulgaris-qcif@users.noreply.github.com>
2022-06-07 17:30:46 +0930

    Feature name query api enhancement (#1088)
    * Add data type date to named query API
    
    * Fix typo and comments and minor refactor
    
    * Change  ReportController to use moment library to

--- typescript/api/controllers/webservice/ReportController.ts
+++ typescript/api/controllers/webservice/ReportController.ts
@@ -29,6 +29,7 @@ declare var User;
 declare var Record;
 declare var _;
 import { APIActionResponse, APIErrorResponse, ListAPIResponse } from '@researchdatabox/redbox-core-types';
+const moment = require('moment');
 /**
  * Package that contains all Controllers.
  */
@@ -93,61 +94,101 @@ export module Controllers {
         if (rows > 100) {
           return this.apiFail(req, res, 400, new APIErrorResponse("Rows must not be greater than 100"));
         }
-        let namedQueryConfig = sails.config.namedQuery[queryName]
+        let namedQueryConfig = sails.config.namedQuery[queryName];
 
         let configMongoQuery = namedQueryConfig.mongoQuery;
-        let mongoQuery = _.clone(configMongoQuery)
+        let mongoQuery = _.clone(configMongoQuery);
         let queryParams = namedQueryConfig.queryParams;
         let paramMap = _.clone(req.query);
 
+        //query params
+        const dataType = 'type';
+        const path = 'path';
+        const queryType = 'queryType';
+        const whenUndefined = 'whenUndefined';
+        const defaultValue = 'defaultValue';
+        const ignore = 'ignore';
+        const ISODate = 'ISODate';
+        const days = 'days';
+        const format = 'format';
+
+        //data types
+        const dateDT = 'date';
+        const numberDT = 'number';
+        const stringDT = 'string';
 
         for (let queryParam in queryParams) {
-          let value = paramMap[queryParam]
-          sails.log.error(`${queryParam} has value ${value}`)
+          
+          let value = paramMap[queryParam];
+          sails.log.error(`${queryParam} has value ${value}`);
           if (value == undefined && queryParams[queryParam].required === true) {
             return this.apiFail(req, res, 400, new APIErrorResponse(`${queryParam} is a required parameter`));
           }
-          if (queryParams[queryParam].type == 'number') {
+          if (queryParams[queryParam][dataType] == numberDT) {
             value = _.toNumber(value)
           }
 
-          if (queryParams[queryParam].type == 'string') {
-            if (!_.isEmpty(queryParams[queryParam].queryType)) {
+          if (queryParams[queryParam][dataType] == stringDT) {
+            if (!_.isEmpty(queryParams[queryParam][queryType])) {
               let query = {}
               // if there is no value pass empty string
 
               if (value == undefined) {
-                if (queryParams[queryParam].whenUndefined == "defaultValue") {
-                  value = queryParams[queryParam].defaultValue
+                if (queryParams[queryParam][whenUndefined] == defaultValue) {
+                  value = queryParams[queryParam][defaultValue];
                 }
               }
-              if (value != undefined || (value ==undefined && queryParams[queryParam].whenUndefined != "ignore")) {
-                query[queryParams[queryParam].queryType] = value;
+              if (value != undefined || (value == undefined && queryParams[queryParam][whenUndefined] != ignore)) {
+                query[queryParams[queryParam][queryType]] = value;
                 value = query;
               }
             }
+          }
 
+          if(queryParams[queryParam][dataType] == dateDT) {
+            if (!_.isEmpty(queryParams[queryParam][queryType])) {
+              let query = {};
+              if (_.isUndefined(value)) {
+                if (queryParams[queryParam][whenUndefined] == defaultValue) {
+                  if(queryParams[queryParam][format] == days) {
+                    let days = _.toInteger(queryParams[queryParam][defaultValue]);
+                    let nowDateAddOrSubtract = moment();
+                    if (days > 0) {
+                      //Going forward in time X number of days
+                      nowDateAddOrSubtract = nowDateAddOrSubtract.add(days, 'days');
+                    } else if(days < 0) {
+                      //This "additional" step makes the code self explanatory
+                      days = days * -1;
+                      //Going backwards in time X number of days
+                      nowDateAddOrSubtract = nowDateAddOrSubtract.subtract(days, 'days');
+                    }
+                    value = nowDateAddOrSubtract.toISOString();
+                  } else if(queryParams[queryParam][format] == ISODate) {
+                    value = queryParams[queryParam][defaultValue];
+                  }
+                  query[queryParams[queryParam][queryType]] = value;
+                  value = query;
+                }
+              }
+            }
           }
           
-          if (value == undefined && queryParams[queryParam].whenUndefined == "ignore") {
+          if (value == undefined && queryParams[queryParam][whenUndefined] == ignore) {
             
-              delete mongoQuery[queryParams[queryParam].path]
-
+              delete mongoQuery[queryParams[queryParam][path]];
           } else {
-            let existingValue = _.get(mongoQuery,queryParams[queryParam].path)
+
+            let existingValue = _.get(mongoQuery,queryParams[queryParam][path])
             if(existingValue != null && _.isObject(existingValue)) {
-              _.merge(value,existingValue)
+              _.merge(value,existingValue);
             }
-            
-            _.set(mongoQuery, queryParams[queryParam].path, value)
-
-           
+            _.set(mongoQuery, queryParams[queryParam][path], value);
           }
-
         }
-        mongoQuery["metaMetadata.brandId"] = brand.id;
+        
+        mongoQuery['metaMetadata.brandId'] = brand.id;
 
-        sails.log.error(mongoQuery)
+        sails.log.error(mongoQuery);
         let totalItems = await Record.count(mongoQuery).meta({
           enableExperimentalDeepTargets: true
         });
