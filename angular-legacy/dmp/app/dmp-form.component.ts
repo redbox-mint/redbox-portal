abac14876a54abe4855a379170ecc62e04209a23
ADBC <alejandro.bulgaris@qcif.edu.au>
2023-01-30 05:39:40 +0000

    Migrate load form in memory changes to latest develop branch

--- angular-legacy/dmp/app/dmp-form.component.ts
+++ angular-legacy/dmp/app/dmp-form.component.ts
@@ -112,7 +112,7 @@ export class DmpFormComponent extends LoadableComponent {
   /**
     The form name to use, by default you don't need to specify this.
    */
-  formName: string;
+  @Input() formName: string;
 
   finishedRendering:boolean;
 
@@ -151,12 +151,18 @@ export class DmpFormComponent extends LoadableComponent {
       this.initSubs.unsubscribe();
       translationService.isReady(tService => {
         this.fieldMap = {_rootComp:this};
+        console.log(this.fieldMap);
+        console.log('================= fieldMap');
         this.oid = elm.nativeElement.getAttribute('oid');
         this.editMode = elm.nativeElement.getAttribute('editMode') == "true";
         this.recordType = elm.nativeElement.getAttribute('recordType');
+        console.log(elm.nativeElement.getAttribute('recordType'));
+        console.log('================= recordType');
         this.needsSave = _.isUndefined(elm.nativeElement.getAttribute('needsSave')) ? false : elm.nativeElement.getAttribute('needsSave') == "true";
         this.formName = elm.nativeElement.getAttribute('formName') || "";
-        console.log(`Loading form with OID: ${this.oid}, on edit mode:${this.editMode}, Record Type: ${this.recordType}`);
+        console.log(elm.nativeElement.getAttribute('formName'));
+        console.log('================= formName');
+        console.log(`Loading form with OID: ${this.oid}, on edit mode:${this.editMode}, Record Type: ${this.recordType}, formName: ${this.formName}`);
         this.RecordsService.getForm(this.oid, this.recordType, this.editMode, this.formName).then((obs:any) => {
           obs.subscribe((form:any) => {
             this.formDef = form;
