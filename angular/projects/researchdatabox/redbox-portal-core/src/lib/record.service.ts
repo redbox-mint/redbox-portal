8573716eb3ee0bacfddeefa04f270e3250d55c74
Shilo Banihit <shiloworks@gmail.com>
2023-02-14 14:53:50 +1000

    feat: Added record service placeholder

---
+++ angular/projects/researchdatabox/redbox-portal-core/src/lib/record.service.ts
@@ -0,0 +1,55 @@
+// Copyright (c) 2023 Queensland Cyber Infrastructure Foundation (http://www.qcif.edu.au/)
+//
+// GNU GENERAL PUBLIC LICENSE
+//    Version 2, June 1991
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import { Injectable, Inject } from '@angular/core';
+import { Subject, firstValueFrom } from 'rxjs';
+import { APP_BASE_HREF } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { mergeMap } from 'rxjs/operators';
+import { Service } from './service.interface';
+import { ConfigService } from './config.service';
+import { UtilityService } from './utility.service';
+import { LoggerService } from './logger.service';
+import { HttpClientService } from './httpClient.service';
+
+/**
+ * Record Service
+ * 
+ * Author: <a href='https://github.com/shilob' target='_blank'>Shilo Banihit</a>
+ */
+@Injectable()
+export class RecordService extends HttpClientService {
+
+  constructor( 
+    @Inject(HttpClient) protected override http: HttpClient, 
+    @Inject(APP_BASE_HREF) public override rootContext: string,
+    @Inject(UtilityService) protected override utilService: UtilityService,
+    @Inject(ConfigService) protected override configService: ConfigService,
+    @Inject(LoggerService) private loggerService: LoggerService
+  ) {
+    super(http, rootContext, utilService, configService);
+  }
+
+  public override async waitForInit(): Promise<any> {
+    await super.waitForInit();
+    this.enableCsrfHeader();
+    return this;
+  }
+
+}
