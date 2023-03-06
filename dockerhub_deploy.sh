1a27637b98228df32164f6734f1168f7711711a3
Shilo Banihit <shiloworks@gmail.com>
2023-03-06 15:01:36 +1000

    fix: replaced "/" with "-" when pushing to docker

--- dockerhub_deploy.sh
+++ dockerhub_deploy.sh
@@ -8,7 +8,10 @@ fi;
 if [ -z "$CIRCLE_BRANCH" ]; then
         export TAG="$CIRCLE_TAG";
 fi;
-
+# Clean up docker tag, replaces '/' with '-'
+if [ ! -z "$TAG" ]; then
+  export TAG=${TAG//\//-}
+fi
 docker build -f Dockerfile -t $REPO:$CIRCLE_SHA1 .
 docker tag $REPO:$CIRCLE_SHA1 $REPO:$TAG
 docker tag $REPO:$CIRCLE_SHA1 $REPO:circleci-$CIRCLE_BUILD_NUM
