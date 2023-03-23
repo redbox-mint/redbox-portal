08b24f37ddf6b8dce87f0020d1d47e3c77bbb79e
Shilo Banihit <shiloworks@gmail.com>
2023-03-23 16:04:29 +1000

    feat: NG15 - Increased reporting app testing coverage

--- angular/projects/researchdatabox/report/src/app/report.component.spec.ts
+++ angular/projects/researchdatabox/report/src/app/report.component.spec.ts
@@ -1,31 +1,114 @@
+import { APP_INITIALIZER, LOCALE_ID } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { APP_BASE_HREF } from '@angular/common'; 
+import { RedboxPortalCoreModule, UtilityService, LoggerService, TranslationService, ConfigService, ReportService, getStubConfigService, getStubTranslationService, appInit, localeId, getStubReportService, ReportFilter, Report, ReportResult, RecordPropViewMeta } from '@researchdatabox/redbox-portal-core';
+import { I18NextModule, I18NEXT_SERVICE } from 'angular-i18next';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { FormsModule } from "@angular/forms";
+import { DateTime } from 'luxon';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { ReportComponent } from './report.component';
 
-describe('AppComponent', () => {
+describe('ReportComponent', () => {
+  let configService:any;
+  let reportService: any;
+  let translationService: any;
+  let mockReportData: any;
+
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
+    configService = getStubConfigService();
+    translationService = getStubTranslationService();
+    const mockReportConfigData = {
+      title: 'Mock Report',
+      name: 'mock-report',
+      filter: [
+        {
+          "paramName": "dateObjectModifiedRange",
+          "type": "date-range",
+          "property": "date_object_modified",
+          "message": "Filter by date modified"
+        }
+      ] as ReportFilter[],
+      columns: [
+        {
+          "label": "Chief Investigator",
+          "property": "contributor_ci.text_full_name",
+          "template" : "${ data['contributor_ci.text_full_name'] }"
+        }
+      ] as RecordPropViewMeta[]
+    } as Report;
+    let pageNum = 1;
+    const mockReportResult = {
+      records: [
+        {
+          id: '123'
+        }
+      ],
+      total: 10,
+      pageNum: pageNum,
+      recordsPerPage: 1
+    } as ReportResult;
+    mockReportData = {
+      reportConfig: mockReportConfigData,
+      reportResult: mockReportResult
+    };
+    reportService = getStubReportService(mockReportData);
+    const testModule = await TestBed.configureTestingModule({
       declarations: [
         ReportComponent
       ],
-    }).compileComponents();
+      imports: [
+        FormsModule,
+        I18NextModule.forRoot(),
+        BsDatepickerModule.forRoot(),
+        PaginationModule.forRoot(),
+        RedboxPortalCoreModule
+      ],
+      providers: [
+        {
+          provide: APP_BASE_HREF,
+          useValue: 'base'
+        },
+        LoggerService,
+        UtilityService,
+        {
+          provide: TranslationService,
+          useValue: translationService
+        },
+        {
+          provide: ConfigService,
+          useValue: configService
+        },
+        {
+          provide: ReportService,
+          useValue: reportService
+        },
+        {
+          provide: APP_INITIALIZER,
+          useFactory: appInit,
+          deps: [I18NEXT_SERVICE],
+          multi: true,
+        },
+        {
+          provide: LOCALE_ID,
+          deps: [I18NEXT_SERVICE],
+          useValue: localeId
+        }
+      ]
+    });
+    TestBed.inject(I18NEXT_SERVICE);
+    await testModule.compileComponents();
   });
 
-  it('should create the app', () => {
+  it('should create the app', async function() {
     const fixture = TestBed.createComponent(ReportComponent);
+    fixture.debugElement.nativeElement.setAttribute('reportName', mockReportData.reportConfig.name);
     const app = fixture.componentInstance;
+    
     expect(app).toBeTruthy();
-  });
+    fixture.autoDetectChanges(true);
+    await app.waitForInit();
+    await fixture.whenStable();
 
-  it(`should have as title '@researchdatabox/report'`, () => {
-    const fixture = TestBed.createComponent(ReportComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('@researchdatabox/report');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(ReportComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('@researchdatabox/report app is running!');
   });
 });
