717244ec38a355062f22b84b4be10c250dc204d1
ci-build <ci-build@redboxresearchdata.com.au>
2023-03-29 06:02:27 +0000

    [ci skip] Updating documents

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -768,6 +768,9 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                         </div>
                         <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
                             <li class="link">
+                                <a href="interfaces/CustomValidationHandlerField.html" data-type="entity-link" >CustomValidationHandlerField</a>
+                            </li>
+                            <li class="link">
                                 <a href="interfaces/UserForm.html" data-type="entity-link" >UserForm</a>
                             </li>
                             <li class="link">
