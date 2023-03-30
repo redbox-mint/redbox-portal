46bce60ac829cb5900e5972aaa25add949358da7
ADBC <alejandro.bulgaris@qcif.edu.au>
2023-03-30 03:32:53 +0000

    Dashboard basic angular tests

--- angular/projects/researchdatabox/dashboard/src/app/dashboard.component.spec.ts
+++ angular/projects/researchdatabox/dashboard/src/app/dashboard.component.spec.ts
@@ -1,26 +1,72 @@
-// import { TestBed } from '@angular/core/testing';
-// import { DashboardComponent } from './dashboard.component';
+import { TestBed } from '@angular/core/testing';
+import { DashboardComponent } from './dashboard.component';
+import { FormsModule } from "@angular/forms";
+import { APP_BASE_HREF } from '@angular/common'; 
+import { I18NextModule, I18NEXT_SERVICE } from 'angular-i18next';
+import { UtilityService, LoggerService, ConfigService, TranslationService, RecordService, UserService } from '@researchdatabox/redbox-portal-core';
+import { getStubConfigService, getStubTranslationService, getStubRecordService, getStubUserService } from '@researchdatabox/redbox-portal-core';
 
-// describe('DashboardComponent', () => {
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [
-//         DashboardComponent
-//       ],
-//     }).compileComponents();
-//   });
+let configService:any;
+let recordService: any;
+let translationService: any;
+let userService: any;
+const username = 'testUser';
+const password = 'some-password';
 
-//   it('should create the app', () => {
-//     const fixture = TestBed.createComponent(DashboardComponent);
-//     const app = fixture.componentInstance;
-//     expect(app).toBeTruthy();
-//   });
+describe('DashboardComponent', () => {
+  beforeEach(async () => {
+    configService = getStubConfigService();
+    translationService = getStubTranslationService();
+    recordService = getStubRecordService();
+    userService = getStubUserService(username, password);
+    const testModule = TestBed.configureTestingModule({
+      declarations: [
+        DashboardComponent
+      ],
+      imports: [
+        FormsModule,
+        I18NextModule.forRoot()
+      ],
+      providers: [
+        {
+          provide: APP_BASE_HREF,
+          useValue: 'base'
+        },
+        LoggerService,
+        UtilityService,
+        {
+          provide: TranslationService,
+          useValue: translationService
+        },
+        {
+          provide: ConfigService,
+          useValue: configService
+        },
+        {
+          provide: RecordService,
+          useValue: recordService
+        },
+        {
+          provide: UserService,
+          useValue: userService
+        }
+      ]
+    });
+    TestBed.inject(I18NEXT_SERVICE);
+    await testModule.compileComponents();
+  });
 
-  // it(`should have as title '@researchdatabox/dashboard'`, () => {
-  //   const fixture = TestBed.createComponent(DashboardComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(dashboard.title).toEqual('@researchdatabox/dashboard');
-  // });
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(DashboardComponent);
+    const dashboardComponent = fixture.componentInstance;
+    expect(dashboardComponent).toBeTruthy();
+  });
+
+  it(`should have as title '@researchdatabox/dashboard'`, () => {
+    const fixture = TestBed.createComponent(DashboardComponent);
+    const dashboardComponent = fixture.componentInstance;
+    expect(dashboardComponent.title).toEqual('@researchdatabox/dashboard');
+  });
 
   // it('should render title', () => {
   //   const fixture = TestBed.createComponent(DashboardComponent);
@@ -28,4 +74,4 @@
   //   const compiled = fixture.nativeElement as HTMLElement;
   //   expect(compiled.querySelector('.content span')?.textContent).toContain('@researchdatabox/dashboard app is running!');
   // });
-// });
+});
