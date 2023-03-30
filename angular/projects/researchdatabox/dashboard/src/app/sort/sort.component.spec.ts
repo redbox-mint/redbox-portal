50dada42abdac0c0db82854714ffd9d3e7461f27
ADBC <alejandro.bulgaris@qcif.edu.au>
2023-03-30 01:21:42 +0000

    Add sort clicked angular test to sort component

--- angular/projects/researchdatabox/dashboard/src/app/sort/sort.component.spec.ts
+++ angular/projects/researchdatabox/dashboard/src/app/sort/sort.component.spec.ts
@@ -1,23 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { SortComponent } from './sort.component';
 
 describe('SortComponent', () => {
-  let component: SortComponent;
-  let fixture: ComponentFixture<SortComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ SortComponent ]
     })
     .compileComponents();
-
-    fixture = TestBed.createComponent(SortComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    const fixture = TestBed.createComponent(SortComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
+  });
+
+  it('sortClicked should toggle sort', () => {
+    const fixture = TestBed.createComponent(SortComponent);
+    const component = fixture.componentInstance;
+    expect(component.sort)
+      .withContext('Empty at first')
+      .toBe('');
+    component.sortClicked();
+    expect(component.sort)
+      .withContext('asc after click')
+      .toBe('asc');
+    component.sortClicked();
+    expect(component.sort)
+      .withContext('desc after second click')
+      .toBe('desc');
   });
 });
