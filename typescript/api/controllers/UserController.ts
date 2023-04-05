4580a102fed9fec864b3f431cc644ff80a54eac5
andrewbrazzatti <andrew@redboxresearchdata.com.au>
2023-04-05 18:38:29 +0930

    Feature: Upgrade Passport to version 0.6 (#1184)
    * Initial commit of working passport 0.6 branch. Disables the sails session hook as it no longer appears to be compatible.
    
    * Made sessions configurable in a config file.
    Disabled websockets as they don't work with redbox session middleware currently and are unused in the application.
    
    * Implemented session initialization in a different way due to issues with accessing the csrfToken endpoint and logging out

--- typescript/api/controllers/UserController.ts
+++ typescript/api/controllers/UserController.ts
@@ -123,16 +123,18 @@ export module Controllers {
         redirUrl = req.session.logoutUrl;
       }
       let user = req.session.user ? req.session.user : req.user;
-      req.logout();
-      UsersService.addUserAuditEvent(user, "logout", requestDetails).then(response => {
-        sails.log.debug(`User logout audit event created: ${_.isEmpty(user) ? '' : user.id}`);
-      }).catch(err => {
-        sails.log.error(`User logout audit event failed`)
-        sails.log.error(err)
+      req.logout(function(err) {
+        if (err) { res.send(500, 'Logout failed'); }
+        UsersService.addUserAuditEvent(user, "logout", requestDetails).then(response => {
+          sails.log.debug(`User logout audit event created: ${_.isEmpty(user) ? '' : user.id}`);
+        }).catch(err => {
+          sails.log.error(`User logout audit event failed`)
+          sails.log.error(err)
+        });
+        // instead of destroying the session, as per M$ directions, we only unset the user, so branding, etc. is retained in the session
+        _.unset(req.session, 'user');
+        res.redirect(redirUrl); 
       });
-      // instead of destroying the session, as per M$ directions, we only unset the user, so branding, etc. is retained in the session
-      _.unset(req.session, 'user');
-      res.redirect(redirUrl); 
     }
 
     public info(req, res) {
