3e94458f43b34b8ea428c13f7496de8047a5c9a4
ADBC <alejandro.bulgaris@qcif.edu.au>
2023-04-12 06:56:45 +0000

    Add basic angular testing for manage users app

--- angular/projects/researchdatabox/manage-users/src/app/manage-users.component.spec.ts
+++ angular/projects/researchdatabox/manage-users/src/app/manage-users.component.spec.ts
@@ -1,31 +1,107 @@
-// import { TestBed } from '@angular/core/testing';
-// import { ManageUsersComponent } from './manage-users.component';
+import { TestBed } from '@angular/core/testing';
+import { ManageUsersComponent } from './manage-users.component';
+import { APP_INITIALIZER, LOCALE_ID } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common'; 
+import { FormsModule, FormBuilder } from "@angular/forms";
+import { I18NextModule, I18NEXT_SERVICE } from 'angular-i18next';
+import { UtilityService, LoggerService, TranslationService, ConfigService, UserService } from '@researchdatabox/portal-ng-common';
+import { getStubConfigService, getStubTranslationService, getStubUserService, appInit, localeId } from '@researchdatabox/portal-ng-common';
 
-// describe('AppComponent', () => {
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [
-//         ManageUsersComponent
-//       ],
-//     }).compileComponents();
-//   });
+let configService:any;
+let userService: any;
+let translationService: any;
+const username = 'testUser';
+const password = 'very-scary-password';
 
-//   it('should create the app', () => {
-//     const fixture = TestBed.createComponent(ManageUsersComponent);
-//     const app = fixture.componentInstance;
-//     expect(app).toBeTruthy();
-//   });
+let rolesData = [
+        {
+            users: [
+                {
+                    username: "admin",
+                    type: "local",
+                    name: "Local Admin"
+                }
+            ],
+            id: "ABC123",
+            name: "Admin"
+        }
+    ];
 
-//   it(`should have as title '@researchdatabox/manage-users'`, () => {
-//     const fixture = TestBed.createComponent(ManageUsersComponent);
-//     const app = fixture.componentInstance;
-//     expect(app.title).toEqual('@researchdatabox/manage-users');
-//   });
+let usersData = [
+        {
+            roles: [
+                {
+                    name: "Admin"
+                }
+            ],
+            username: "admin",
+            type: "local",
+            name: "Local Admin"
+        }
+   ];
 
-//   it('should render title', () => {
-//     const fixture = TestBed.createComponent(ManageUsersComponent);
-//     fixture.detectChanges();
-//     const compiled = fixture.nativeElement as HTMLElement;
-//     expect(compiled.querySelector('.content span')?.textContent).toContain('@researchdatabox/manage-users app is running!');
-//   });
-// });
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    configService = getStubConfigService();
+    translationService = getStubTranslationService();
+    userService = getStubUserService(username, password, {}, rolesData, usersData);
+    const testModule = TestBed.configureTestingModule({
+      declarations: [
+        ManageUsersComponent
+      ],
+      imports: [
+        FormsModule,
+        I18NextModule.forRoot()
+      ],
+      providers: [
+        FormBuilder,
+        {
+          provide: APP_BASE_HREF,
+          useValue: 'base'
+        },
+        LoggerService,
+        UtilityService,
+        {
+          provide: TranslationService,
+          useValue: translationService
+        },
+        {
+          provide: ConfigService,
+          useValue: configService
+        },
+        {
+          provide: UserService,
+          useValue: userService
+        },
+        {
+          provide: APP_INITIALIZER,
+          useFactory: appInit,
+          deps: [I18NEXT_SERVICE],
+          multi: true,
+        },
+        {
+          provide: LOCALE_ID,
+          deps: [I18NEXT_SERVICE],
+          useValue: localeId
+        }
+      ]
+    });
+    
+    TestBed.inject(FormBuilder);
+    TestBed.inject(UserService);
+    TestBed.inject(I18NEXT_SERVICE);
+    await testModule.compileComponents();
+  });
+
+  it('should create the app', async () =>  {
+    const fixture = TestBed.createComponent(ManageUsersComponent);
+    const app = fixture.componentInstance;
+    fixture.autoDetectChanges(true);
+    expect(app).toBeTruthy();
+    await app.waitForInit();
+    await fixture.whenStable();
+    expect(app.allRoles.length).toBeGreaterThan(0);
+    expect(app.allUsers.length).toBeGreaterThan(0);
+  });
+
+});
