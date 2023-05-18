239f98de584b96c4dae619d13efe1877773232c9
Shilo Banihit <shiloworks@gmail.com>
2023-05-18 12:13:09 +1000

    Initial skeleton for supporting dynamic rendering of form fields as well as supporting custom form components.

---
+++ angular/projects/researchdatabox/form/src/app/component/textfield.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormBaseComponent, ModelBase } from "@researchdatabox/portal-ng-common";
+
+export class TextField extends ModelBase<string> {
+  type: string = 'text'
+  constructor(initJson: any) {
+    super(initJson);
+    
+  }
+}
+
+@Component({
+  selector: 'textfield',
+  template: `
+  <span>Text Label</span>
+  <input type='text' value='hello!' />
+  `,
+})
+export class TextFieldComponent extends FormBaseComponent {
+
+}
