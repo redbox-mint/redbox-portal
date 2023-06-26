0a8e285318e71341e9f4eecdc9f00986f32d5dec
ADBC <alejandro.bulgaris@qcif.edu.au>
2023-06-26 05:04:40 +0000

    Add parse object otion to run templates method in rdmp service

--- typescript/api/services/RDMPService.ts
+++ typescript/api/services/RDMPService.ts
@@ -660,6 +660,12 @@ export module Services {
       sails.log.verbose(`runTemplates oid: ${oid} with user: ${JSON.stringify(user)}`);
       sails.log.verbose(JSON.stringify(record));
 
+      let parseObject = false;
+      let tmpParseObject = _.get(options, 'parseObject');
+      if(!_.isUndefined(tmpParseObject)){
+        parseObject = tmpParseObject;
+      }
+
       let tmplConfig = null;
       try {
         _.each(options.templates, (templateConfig) => {
@@ -676,7 +682,12 @@ export module Services {
             imports: imports
           };
           const data = _.template(templateConfig.template, templateData)();
-          _.set(record, templateConfig.field, data);
+          if(parseObject) {
+            let obj = JSON.parse(data);
+            _.set(record, templateConfig.field, obj);
+          } else {
+            _.set(record, templateConfig.field, data);
+          }
         });
       } catch (e) {
         const errLog = `Failed to run one of the string templates: ${JSON.stringify(tmplConfig)}`
