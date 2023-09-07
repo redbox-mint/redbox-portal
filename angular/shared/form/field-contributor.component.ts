c25381eec39f3c05c7f7da8f8185d4815cef1b77
Andrew Brazzatti <a.brazzatti@qcif.edu.au>
2023-09-07 02:23:46 +0000

    Fixed issue where on re-showing the contributor component the autocomplete field's label is lost

--- angular/shared/form/field-contributor.component.ts
+++ angular/shared/form/field-contributor.component.ts
@@ -98,6 +98,7 @@ export class ContributorField extends FieldBase<any> implements CustomValidation
 
     this.roles = options['roles'] || [];
     this.value = options['value'] || this.setEmptyValue();
+    
 
     this.activeValidators = options['activeValidators'];
 
@@ -274,7 +275,6 @@ export class ContributorField extends FieldBase<any> implements CustomValidation
     } else {
       this.toggleConditionalValidation(!_.isEmpty(value.text_full_name));
     }
-
   }
 
   toggleConditionalValidation(hasValue) {
@@ -490,6 +490,68 @@ export class ContributorField extends FieldBase<any> implements CustomValidation
       });
     }
   }
+
+  public setVisibility(data, eventConf:any = {}) {
+    let newVisible = this.visible;
+    if (_.isArray(this.visibilityCriteria)) {
+      // save the value of this data in a map, so we can run complex conditional logic that depends on one or more fields
+      if (!_.isEmpty(eventConf) && !_.isEmpty(eventConf.srcName)) {
+        this.subscriptionData[eventConf.srcName] = data;
+      }
+      // only run the function set if we have all the data...
+      if (_.size(this.subscriptionData) == _.size(this.visibilityCriteria)) {
+        newVisible = true;
+        _.each(this.visibilityCriteria, (visibilityCriteria) => {
+          const dataEntry = this.subscriptionData[visibilityCriteria.fieldName];
+          newVisible = newVisible && this.execVisibilityFn(dataEntry, visibilityCriteria);
+        });
+
+      }
+    } else
+    if (_.isObject(this.visibilityCriteria) && _.get(this.visibilityCriteria, 'type') == 'function') {
+      newVisible = this.execVisibilityFn(data, this.visibilityCriteria);
+    } else {
+      newVisible = _.isEqual(data, this.visibilityCriteria);
+    }
+    const that = this;
+    setTimeout(() => {
+      if (!newVisible) {
+        if (that.visible) {
+          // remove validators
+          if (that.formModel) {
+            if(that['disableValidators'] != null && typeof(that['disableValidators']) == 'function') {
+              that['disableValidators']();
+            } else {
+              that.formModel.clearValidators();
+            }
+            that.formModel.updateValueAndValidity();
+            
+          }
+        }
+      } else {
+        if (!that.visible) {
+          // restore validators
+          if (that.formModel) {       
+              if(that['enableValidators'] != null && typeof(that['enableValidators']) == 'function') {
+                that['enableValidators']();
+              } else {
+                that.formModel.setValidators(that.validators);
+              }
+              setTimeout(() => {
+                that.setValue(that.formModel.value,false,true)
+              });
+              that.formModel.updateValueAndValidity();
+          }
+        }
+      }
+      that.visible = newVisible;
+    });
+    if(eventConf.returnData == true) {
+      return data;
+    }
+    
+  }
+
 }
 
 @Component({
