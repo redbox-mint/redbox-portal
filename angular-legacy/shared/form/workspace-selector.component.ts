c73c44fdf09694c180aeb09b0195294b815728c9
Shilo <shiloworks@gmail.com>
2024-01-29 15:36:41 +1000

    Fixed: workspaces selector component card list view

--- angular-legacy/shared/form/workspace-selector.component.ts
+++ angular-legacy/shared/form/workspace-selector.component.ts
@@ -82,35 +82,35 @@ export class WorkspaceSelectorComponent extends SimpleComponent {
         <!-- Begin conversion card-deck -->
         <div class="card-deck">
           <div class="card workspaces col-lg-4 col-md-5 col-sm-6 col-xs-12"  style="display: flex; flex-direction: column;width: 30rem;" *ngFor="let workspaceType of field.workspaceApps">
-          <div class="panel panel-default">
-            <div class="panel-heading">
-              <h5 class="panel-title card-title" style="margin-top: 1px">
-                <span>{{ workspaceType.label | uppercase }}</span>
-              </h5>
-            </div>
-            <div class="panel-body workspaces-panel-body">
-              <img class="card-img-top" src="{{ workspaceType.logo }}" alt="{{workspaceType.name}}">
-              <div class="card-body" style="margin-bottom: auto;">
-                <h5 class="card-title" style="margin-top: 2px">
-                  <span>{{ workspaceType.subtitle }}</span>
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h5 class="panel-title card-title" style="margin-top: 1px">
+                  <span>{{ workspaceType.label | uppercase }}</span>
                 </h5>
-                <p class="card-text">{{ workspaceType.description }}</p>
               </div>
-            </div>
-            <div class="card-footer" style="margin-top: auto;">
-              <ng-container *ngIf="field.allowAdd">
-                <button type='button' (click)="saveAndOpenWorkspace(workspaceType)"  class="btn btn-block btn-primary">{{ field.open }}</button>
-              </ng-container>
-              <ng-container *ngIf="!field.allowAdd">
-                <p class="text-danger">
-                  <strong>{{ field.saveFirst }}</strong>
-                </p>
-              </ng-container>
+              <div class="panel-body workspaces-panel-body">
+                <img class="card-img-top" src="{{ workspaceType.logo }}" alt="{{workspaceType.name}}">
+                <div class="card-body" style="margin-bottom: auto;">
+                  <h5 class="card-title" style="margin-top: 2px">
+                    <span>{{ workspaceType.subtitle }}</span>
+                  </h5>
+                  <p class="card-text">{{ workspaceType.description }}</p>
+                </div>
+              </div>
+              <div class="card-footer" style="margin-top: auto;">
+                <ng-container *ngIf="field.allowAdd">
+                  <button type='button' (click)="saveAndOpenWorkspace(workspaceType)"  class="btn btn-block btn-primary">{{ field.open }}</button>
+                </ng-container>
+                <ng-container *ngIf="!field.allowAdd">
+                  <p class="text-danger">
+                    <strong>{{ field.saveFirst }}</strong>
+                  </p>
+                </ng-container>
+              </div>
             </div>
           </div>
         </div>
-        </div>
-        <!-- End conversion card-deck -> 
+        <!-- End conversion card-deck --> 
       </div>
     </ng-container>
   </div>
