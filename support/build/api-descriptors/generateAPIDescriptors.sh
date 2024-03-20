c6d83926a866e2fc75a3763e4d78019b14a57b9d
andrewbrazzatti <andrew@redboxresearchdata.com.au>
2024-03-20 11:27:44 +1030

    Feature: OpenAPI descriptors (#1997)
    * Added script to generate OpenAPI descriptor templates and added the command to the CircleCI build
    
    * Added baseUrl parameter to context to correctly evaluate the api blueprint template for doc generation
    
    * Changed api descriptor generation to use a local npm install of the tools as the docker image used by the build doesn't allow global installation
    
    * Fixed path to api-descriptor generation
    
    * Fixed another typo in script path

---
+++ support/build/api-descriptors/generateAPIDescriptors.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+npm install
+
+node_modules/.bin/ejs-cli ../../../views/default/default/apidocsapib.ejs -O '{"portal":"rdmp","branding":"default", "baseUrl": "https://demo.redboxresearchdata.com.au"}' > apidocs.apib 
+node_modules/.bin/apib2swagger -i apidocs.apib --yaml --open-api-3 --bearer-apikey > ../../../views/default/default/apidocsswaggeryaml.ejs
+node_modules/.bin/apib2swagger -i apidocs.apib --open-api-3 --bearer-apikey > ../../../views/default/default/apidocsswaggerjson.ejs
+sed -i 's#/default/rdmp#/\<%= branding %>/\<%= portal %>#g' ../../../views/default/default/apidocsswaggeryaml.ejs
+sed -i 's#https://demo.redboxresearchdata.com.au#<%= baseUrl %>#g' ../../../views/default/default/apidocsswaggeryaml.ejs
+sed -i 's#/default/rdmp#/\<%= branding %>/\<%= portal %>#g' ../../../views/default/default/apidocsswaggerjson.ejs
+sed -i 's#https://demo.redboxresearchdata.com.au#<%= baseUrl %>#g' ../../../views/default/default/apidocsswaggerjson.ejs
+rm apidocs.apib
+rm -Rf node_modules
