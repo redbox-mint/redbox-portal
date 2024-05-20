d78c59413e53c5c31f009c5238664cdbc31eb584
alejandro.bulgaris@qcif.edu.au <alejandro.bulgaris@qcif.edu.au>
2024-05-20 06:36:59 +0000

    Update searchAdvanced method in SearchService interface

--- core/src/SearchService.ts
+++ core/src/SearchService.ts
@@ -1,7 +1,7 @@
 export interface SearchService{
 
   index(id:string, data:any):any;
-  searchFuzzy(type, workflowState, searchQuery, exactSearches, facetSearches, brand, user, roles, returnFields, start?:number, rows?:number): Promise<any>;
+  searchFuzzy(type: string, workflowState: string, searchQuery: string, exactSearches: any, facetSearches: any, brand: any, user: any, roles: any, returnFields: any, start?:number, rows?:number): Promise<any>;
   remove(id: string): any;
-  searchAdvanced(query): Promise<any>;
+  searchAdvanced(type: string, query: string): Promise<any>;
 }
