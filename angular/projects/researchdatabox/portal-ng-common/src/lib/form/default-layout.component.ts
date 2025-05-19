c654145cf72ddde528cad89d0afc27d8184f0c25
shilob <shiloworks@gmail.com>
2025-05-19 14:54:16 +1000

    Pass on templates to form field components, refactored base component init logic (#2978)
    * feat: enhance form components with template reference handling, modified component instantion to "createComponent"
    
    * feat: enhance form field base component with improved initialization and readiness handling, allowing child classes to override specific lifecycle stages

--- angular/projects/researchdatabox/portal-ng-common/src/lib/form/default-layout.component.ts
+++ angular/projects/researchdatabox/portal-ng-common/src/lib/form/default-layout.component.ts
@@ -1,7 +1,8 @@
 import { FormFieldBaseComponent, FormFieldCompMapEntry } from './form-field-base.component';
 import { FormComponentLayoutDefinition } from './config.model';
 import { isEmpty as _isEmpty } from 'lodash-es';
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef, TemplateRef, ComponentRef } from '@angular/core';
+import { FormBaseWrapperComponent } from './base-wrapper.component';
 /**
  * Default Form Component Layout
  * 
@@ -26,19 +27,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'redbox-form-default-component-layout',
   template: `
-  <ng-container *ngIf="model && componentDefinition"> 
-    <ng-container *ngIf="componentDefinition?.config?.label">
-    <label>
-      <span [innerHtml]="componentDefinition?.config?.label"></span>
-      <span class="form-field-required-indicator" [innerHTML]="componentDefinition?.config?.labelRequiredStr"></span>
-      <button type="button" class="btn btn-default" *ngIf="componentDefinition?.config?.helpText" (click)="toggleHelpTextVisibility()" [attr.aria-label]="'help' | i18next ">
-      <span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span>
-      </button>
-    </label>
-    <span class="help-block" *ngIf="helpTextVisible" [innerHtml]="componentDefinition?.config?.helpText"></span>
-    </ng-container>
-    <redbox-form-base-wrapper *ngIf="componentClass" [model]="model" [componentClass]="componentClass" [formFieldCompMapEntry]="formFieldCompMapEntry" ></redbox-form-base-wrapper>
-  </ng-container>
+  @if (model && componentDefinition) {
+    @if (componentDefinition.config?.label) {
+      <label>
+        <span [innerHtml]="componentDefinition?.config?.label"></span>
+        <span class="form-field-required-indicator" [innerHTML]="componentDefinition?.config?.labelRequiredStr"></span>
+        @if (componentDefinition.config?.helpText) {
+          <button type="button" class="btn btn-default" (click)="toggleHelpTextVisibility()" [attr.aria-label]="'help' | i18next ">
+          <span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span>
+          </button>
+        }
+      </label>
+      @if (helpTextVisible) {
+        <span class="help-block" [innerHtml]="componentDefinition?.config?.helpText"></span>
+      }
+    }
+    <ng-container #componentContainer>
+    </ng-container> 
+    <!-- instead of rendering the 'before' and 'after' templates around the componentContainer, we supply named templates so the component can render these as it sees fit -->
+    <ng-template #beforeComponentTemplate>
+      Before, is help showing:  {{ helpTextVisible }}
+    </ng-template>
+    <ng-template #afterComponentTemplate>
+      After, is help showing:  {{ helpTextVisible }}
+    </ng-template>
+  }
   `,
   standalone: false,
   // Note: No need for host property here if using @HostBinding
@@ -48,11 +61,44 @@ export class DefaultLayoutComponent<ValueType> extends FormFieldBaseComponent<Va
   componentClass?: typeof FormFieldBaseComponent | null;
   public override componentDefinition?: FormComponentLayoutDefinition;
 
-  override async initComponent(formFieldCompMapEntry: FormFieldCompMapEntry | null) {
-    await super.initComponent(formFieldCompMapEntry);
+  @ViewChild('componentContainer', { read: ViewContainerRef, static: false })
+  componentContainer!: ViewContainerRef;
+
+  @ViewChild('beforeComponentTemplate', { read: TemplateRef, static: false })
+  beforeComponentTemplate!: TemplateRef<any>;
+  @ViewChild('afterComponentTemplate', { read: TemplateRef, static: false })
+  afterComponentTemplate!: TemplateRef<any>;
+
+  wrapperComponentRef!: ComponentRef<FormBaseWrapperComponent<unknown>>;
+
+  /**
+   * Override to set additional properties required by the wrapper component.
+   * 
+   * @param formFieldCompMapEntry 
+   */
+  protected override setPropertiesFromComponentMapEntry(formFieldCompMapEntry: FormFieldCompMapEntry): void {
+    super.setPropertiesFromComponentMapEntry(formFieldCompMapEntry);
     this.componentClass = formFieldCompMapEntry?.componentClass;
     this.componentDefinition = formFieldCompMapEntry?.compConfigJson?.layout as FormComponentLayoutDefinition;
-    
+  }
+  /**
+   * Override what it takes to get the component to be 'ready'
+   */
+  protected override async setComponentReady(): Promise<void> {
+    // Set all the bound properties to the component
+    this.wrapperComponentRef = this.componentContainer.createComponent(FormBaseWrapperComponent);
+    this.wrapperComponentRef.instance.componentClass = this.componentClass;
+    this.wrapperComponentRef.instance.model = this.model;
+    this.wrapperComponentRef.instance.formFieldCompMapEntry = this.formFieldCompMapEntry;
+    if (this.formFieldCompMapEntry && this.beforeComponentTemplate && this.afterComponentTemplate) {
+      this.formFieldCompMapEntry.componentTemplateRefMap = {
+        before: this.beforeComponentTemplate,
+        after: this.afterComponentTemplate
+      };
+    }
+    this.wrapperComponentRef.changeDetectorRef.detectChanges();
+    // finally set the status to 'READY'
+    await super.setComponentReady();
   }
 
   toggleHelpTextVisibility() {
