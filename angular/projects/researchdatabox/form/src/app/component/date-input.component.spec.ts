ea8501d1e573616f25358dd4a53bcf23dcaf2037
Alejandro Bulgaris <alejandro.bulgaris@qcif.edu.au>
2025-10-14 06:11:28 +0000

    Add test that sets value instead of default value

--- angular/projects/researchdatabox/form/src/app/component/date-input.component.spec.ts
+++ angular/projects/researchdatabox/form/src/app/component/date-input.component.spec.ts
@@ -18,7 +18,7 @@ describe('DateInputComponent', () => {
     expect(component).toBeDefined();
   });
 
-  it('should render Date input component', async () => {
+  it('should render Date input component from default value', async () => {
     const formConfig: FormConfig = {
       debugValue: true,
       defaultComponentConfig: {
@@ -32,6 +32,37 @@ describe('DateInputComponent', () => {
             class: 'DateInputModel',
             config: {
               defaultValue: new Date('2025-08-10T10:00:00Z')
+            },
+          },
+          component: {
+            class: 'DateInputComponent',
+            config: {
+            },
+          },
+        },
+      ],
+    };
+
+    const {fixture} = await createFormAndWaitForReady(formConfig);
+    const compiled = fixture.nativeElement as HTMLElement;
+    const inputElement = compiled.querySelector('input[type="text"]');
+    expect((inputElement as HTMLInputElement).value).toEqual('10/08/2025');
+  });
+
+  it('should render Date input component from value', async () => {
+    const formConfig: FormConfig = {
+      debugValue: true,
+      defaultComponentConfig: {
+        defaultComponentCssClasses: 'row',
+      },
+      editCssClasses: 'redbox-form form',
+      componentDefinitions: [
+        {
+          name: 'date_test',
+          model: {
+            class: 'DateInputModel',
+            config: {
+              value: new Date('2025-08-10T10:00:00Z')
             },
           },
           component: {
