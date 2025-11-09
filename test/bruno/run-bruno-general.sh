7ff9e7d5f5f82b93fa6ec0b48b67d4b131464063
Mark Cottman-Fields <mark.cottmanfields@qcif.edu.au>
2025-11-09 23:36:56 +0000

    work in progress fixing bruno test runner

---
+++ test/bruno/run-bruno-general.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -o xtrace
+
+cd /opt/bruno-tests
+
+npm install --ignore-scripts --strict-peer-deps
+rm /opt/junit/backend-bruno/backend-bruno-general.xml || true
+exec node_modules/.bin/bru run --env int-test --format junit --bail \
+ --output /opt/junit/backend-bruno/backend-bruno-general.xml
--- test/bruno/run-bruno-oidc.sh
+++ test/bruno/run-bruno-oidc.sh
@@ -6,8 +6,6 @@ set -o xtrace
 cd /opt/bruno-tests
 
 npm install --ignore-scripts --strict-peer-deps
-
-ls -la node_modules/.bin/
-
+rm /opt/junit/backend-bruno/backend-bruno-oidc.xml || true
 exec node_modules/.bin/bru run --env int-test --format junit --bail \
- --output /opt/junit/backend-bruno/backend-bruno-general.xml
+ --output /opt/junit/backend-bruno/backend-bruno-oidc.xml
