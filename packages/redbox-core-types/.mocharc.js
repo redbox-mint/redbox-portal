5888f9d38394d21f237cc9fd600060789bd915f1
Mark Cottman-Fields <mark.cottmanfields@qcif.edu.au>
2026-01-21 04:32:53 +0000

    include redbox-core-types test stats in circleci

--- packages/redbox-core-types/.mocharc.js
+++ packages/redbox-core-types/.mocharc.js
@@ -4,3 +4,14 @@ module.exports = {
     require: ['ts-node/register', 'test/setup.ts'],
     timeout: 5000
 };
+
+if (process.env.CI === "true") {
+  console.log("Mocha running in CI.");
+  // (For CI) Run mocha and write results to a junit format file:
+  module.exports["reporter"] = "mocha-junit-reporter";
+  module.exports["reporter-option"] = "mochaFile=support/junit/mocha.xml";
+} else {
+  console.log("Mocha running in local dev.");
+  // (For development) Run mocha and show the results on stdout:
+  module.exports["reporter"] = "spec";
+}
