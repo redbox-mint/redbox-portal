44c6b22ea01b6b09d2616779e84c5731d123b803
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2026-02-06 02:42:22 +0000

    refactor: Update registerHelper type signature to use 'any' for arguments

--- packages/sails-ng-common/src/handlebars-helpers.ts
+++ packages/sails-ng-common/src/handlebars-helpers.ts
@@ -341,7 +341,7 @@ export type HandlebarsHelperDefinitions = typeof handlebarsHelperDefinitions;
  * @param Handlebars The Handlebars instance to register helpers on
  */
 export function registerSharedHandlebarsHelpers(Handlebars: {
-    registerHelper: (name: string, fn: (...args: unknown[]) => unknown) => void;
+    registerHelper: (name: string, fn: (...args: any[]) => any) => void;
 }): void {
     for (const [name, fn] of Object.entries(handlebarsHelperDefinitions)) {
         Handlebars.registerHelper(name, fn);
