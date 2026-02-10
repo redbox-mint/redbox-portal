65de77bc5a8e5e83c0e194e358bb3fca626be7f7
Mark Cottman-Fields <mark.cottmanfields@qcif.edu.au>
2026-02-10 06:55:34 +0000

    first draft of question tree definition structure

--- packages/sails-ng-common/src/config/component/question-tree.outline.ts
+++ packages/sails-ng-common/src/config/component/question-tree.outline.ts
@@ -23,13 +23,136 @@ import {
     FieldModelDefinitionKindType, FormComponentDefinitionFrameKindType, FormComponentDefinitionKindType
 } from "../shared.outline";
 
+/* QuestionTree data structure */
+
+/**
+ * The available outcomes for a question tree.
+ */
+export interface QuestionTreeOutcomes {
+    /**
+     * The available outcome property keys.
+     */
+    [key: string]: {
+        /**
+         * The key is an available outcome property value.
+         * The optional value is the translation message id.
+         * Set value 'falsy' to use a translation message id of `${outcome-key}-${outcome-value}`.
+         */
+        [key: string]: string | null,
+    }
+}
+type QuestionTreeOutcomePropKeys = keyof QuestionTreeOutcomes;
+
+/**
+ * One answer to a question in a question tree.
+ */
+export interface QuestionTreeQuestionAnswer {
+    /**
+     * The optional translation message to use as the label, or the actual label text.
+     * Leave out or set 'falsy' to use a translation message id of `${question-id}-${answer-id}`.
+     */
+    label?: string | null;
+    /**
+     * The optional outcome applied when this answer is selected.
+     * The 'outcomes' object specifies the valid keys and values that can be used here.
+     * Setting an outcome may not finish the question tree, as it is possible to have multiple outcomes.
+     */
+    outcome?: { [key in QuestionTreeOutcomePropKeys]: string };
+}
+
+/**
+ * The possible answers to a question.
+ */
+export interface QuestionTreeQuestionAnswers {
+    /**
+     * The key is the answer value.
+     */
+    [key: string]: QuestionTreeQuestionAnswer;
+}
+
+type QuestionTreeQuestionAnswerValues = keyof QuestionTreeQuestionAnswers;
+
+/**
+ * A condition to fulfil to show the question.
+ */
+export interface QuestionTreeQuestionRule {
+    /**
+     * The match strategy to apply:
+     * - 'all': all items must match
+     * - 'any': at least one item much match
+     */
+    match: "all" | "any";
+    /**
+     * Each key value pair is a match to check.
+     * The key is a question id, the value is an answer value.
+     */
+    items: { [key in QuestionTreeQuestionIds]: QuestionTreeQuestionAnswerValues };
+}
+
+export interface QuestionTreeQuestions {
+    /**
+     * A question definition.
+     * The key is the question id.
+     */
+    [key: string]: {
+        /**
+         * The minimum number of answers that can be supplied.
+         * Must be at least 1, up to the maximum or the number of answers.
+         */
+        answersMin: number;
+        /**
+         * The maximum number of answers that can be supplied.
+         * Must be at least the minimum, up to the number of answers.
+         */
+        answersMax: number;
+        /**
+         * The possible answers to the question.
+         */
+        answers: QuestionTreeQuestionAnswers;
+        /**
+         * The rules for showing this question.
+         */
+        rules: QuestionTreeQuestionRule[];
+    }
+}
+
+type QuestionTreeQuestionIds =  keyof QuestionTreeQuestions;
+
+const example: QuestionTreeFieldComponentConfigFrame = {
+    outcomes: {
+        prop1: {
+            "value1": "@outcomes-prop1-value1",
+            "value2": "@outcomes-prop1-value2",
+        },
+        prop2: {
+            "value1": "@outcomes-prop2-value1",
+            "value2": "@outcomes-prop2-value2",
+        },
+    },
+    questions: {
+        "question-1": {
+            answersMin: 1,
+            answersMax: 1,
+            answers: {
+                "yes": {label: "@answer-yes", outcome: {prop1: "value1", prop2: "value2"}},
+                "no": {label: "No"},
+            },
+            rules: [
+                {match: "all", items: {"question-2": "no", "question-3": "yes"}},
+                {match: "any", items: {"question-2": "no", "question-3": "yes"}},
+            ],
+        },
+    },
+};
+
 /* QuestionTree Component */
 
 export const QuestionTreeComponentName = `QuestionTreeComponent` as const;
 export type QuestionTreeComponentNameType = typeof QuestionTreeComponentName;
 
 export interface QuestionTreeFieldComponentConfigFrame extends FieldComponentConfigFrame {
-
+    outcomes: QuestionTreeOutcomes;
+    questions: QuestionTreeQuestions;
 }
 
 export interface QuestionTreeFieldComponentConfigOutline extends QuestionTreeFieldComponentConfigFrame, FieldComponentConfigOutline {
