b7d0b0a0d49c3c06d3a953284f086b91305017f5
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2026-02-13 13:58:23 +1030

    fix: update sanitization method to use DomSanitizerService directly

--- packages/redbox-core-types/src/visitor/validator.visitor.ts
+++ packages/redbox-core-types/src/visitor/validator.visitor.ts
@@ -394,7 +394,7 @@ export class ValidatorFormConfigVisitor extends FormConfigVisitor {
         }
 
 
-        const sanitized = getDomSanitizerService().sanitizeWithProfile(value, 'html');
+        const sanitized = DomSanitizerService.sanitizeWithProfile(value, 'html');
         // TODO: Validating this way that the html/markdown content has been sanitized is likely to be brittle
         // Initial implementation is to silently sanitize and warn, but may want to change to rejecting the content in future if sanitization issues are common.
         // This will likely need to hook more into domPurify to be more robust to detect when it is sanitizing content.
