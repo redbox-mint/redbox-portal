cad0bbc3af554482b0a834156279bd0319980260
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2026-05-07 09:56:11 +0930

    test: refactor AgendaQueueService test for improved reliability and clarity

--- test/integration/services/AgendaService.test.ts
+++ test/integration/services/AgendaService.test.ts
@@ -1,8 +1,11 @@
 
 describe('The Agenda Queue Service', function () {
+  let processedTestData = null;
+
   before(function (done) {
-    let testQueueConsumer = function(job) {
+    let testQueueConsumer = function (job) {
       let data = job.attrs.data;
+      processedTestData = data;
       sails.log.debug(`Received ${data}`)
       return data;
     };
@@ -10,22 +13,24 @@ describe('The Agenda Queue Service', function () {
     done();
   });
 
-  it("Send a job and check it's executed", function (done) {    
-    this.timeout(5000);
+  it("Send a job and check it's executed", async function () {
+    this.timeout(30000);
+    processedTestData = null;
 
-    
-    AgendaQueueService.now('testJob',  'Test data' )
-      // TODO: This 2 second waiting to check is a bit of a hack. It's probably robust enough because the dummy job 
-      // that doesn't do anything but there's a chance of random failures due to hardware blocking on the CI server 
-      // This is mitigated a bit by the fact any other test that takes longer than 2 seconds will fail
-     setTimeout(function(){
-      AgendaQueueService.jobs().then(response =>{
-        let job = response[0]
-        //Check that the job has turned up in the queue and that it finished
-        expect(job['attrs']).to.have.property("lastFinishedAt")
-        done();
-    });}, 2000)
-    
+    AgendaQueueService.now('testJob', 'Test data');
+
+    const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+    const maxAttempts = 40;
+
+    for (let attempt = 0; attempt < maxAttempts; attempt++) {
+      if (processedTestData === 'Test data') {
+        return;
+      }
+
+      await wait(500);
+    }
+
+    throw new Error('Timed out waiting for testJob to finish processing in Agenda queue.');
   });
 
 
