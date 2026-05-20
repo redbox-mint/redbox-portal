ad3bddee057a80a9f951c36a2219832b6499db59
shilob <shiloworks@gmail.com>
2026-05-20 15:47:06 +1000

    Bugfix: Sync checkbox tree display after model value updates (#4080)
    * Feature: Add syncDisplayFromModel method to update display after model changes
    
    * Guard checkbox tree display sync

--- angular/projects/researchdatabox/form/src/app/component/checkbox-tree.component.ts
+++ angular/projects/researchdatabox/form/src/app/component/checkbox-tree.component.ts
@@ -232,6 +232,8 @@ export class CheckboxTreeComponent extends FormFieldBaseComponent<CheckboxTreeMo
   private readonly loadingNodeIds = new Set<string>();
   private readonly loadedNodeIds = new Set<string>();
   private modelSubscriptionInitialised = false;
+  private displaySyncInFlight?: Promise<void>;
+  private displaySyncQueued = false;
 
   private leafOnly = true;
   private inlineVocab = false;
@@ -700,6 +702,29 @@ export class CheckboxTreeComponent extends FormFieldBaseComponent<CheckboxTreeMo
     this.formControl.valueChanges
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe(() => this.syncSelectionFromModel());
+  }
+
+  // Invoked by the framework after expression-driven model.value updates,
+  // which use emitEvent:false and therefore bypass the valueChanges subscription.
+  public async syncDisplayFromModel(): Promise<void> {
+    if (this.displaySyncInFlight) {
+      this.displaySyncQueued = true;
+      return this.displaySyncInFlight;
+    }
+    this.displaySyncInFlight = this.runDisplaySyncFromModel();
+    return this.displaySyncInFlight;
+  }
+
+  private async runDisplaySyncFromModel(): Promise<void> {
+    try {
+      do {
+        this.displaySyncQueued = false;
+        this.syncSelectionFromModel();
+        await this.expandToSelectedNodes();
+      } while (this.displaySyncQueued);
+    } finally {
+      this.displaySyncInFlight = undefined;
+    }
   }
 
   private describeLoadError(error: unknown): string {
