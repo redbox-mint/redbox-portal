e2e0a50764aafdb60d751e4b8df1dff4e6f0979b
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2026-07-11 12:37:33 +0930

    fix: use shared Handlebars compiler in Oni templating

--- packages/redbox-core/src/services/oni-v2/templating.ts
+++ packages/redbox-core/src/services/oni-v2/templating.ts
@@ -1,7 +1,6 @@
 import _ from 'lodash';
-import Handlebars from 'handlebars';
 import { Context, Effect, Layer } from 'effect';
-import { jsonataCompileAndEvaluate, registerSharedHandlebarsHelpers } from '@researchdatabox/sails-ng-common';
+import { handlebarsCompile, jsonataCompileAndEvaluate } from '@researchdatabox/sails-ng-common';
 import type { AnyRecord } from './types';
 import { validateSafeHandlebarsTemplate } from '../integration-v2/bindings';
 
@@ -13,23 +12,13 @@ export interface OniTemplatingService {
 
 export const OniTemplatingServiceTag = Context.GenericTag<OniTemplatingService>('redbox/OniTemplatingService');
 
-let helpersRegistered = false;
-
-function ensureHelpers(): void {
-  if (!helpersRegistered) {
-    registerSharedHandlebarsHelpers(Handlebars);
-    helpersRegistered = true;
-  }
-}
-
 export const liveOniTemplatingService: OniTemplatingService = {
   path: (path, context, defaultValue) => Effect.sync(() => _.get(context, path, defaultValue)),
   handlebars: (template, context) =>
     Effect.try({
       try: () => {
-        ensureHelpers();
         validateSafeHandlebarsTemplate(template, 'Oni');
-        return Handlebars.compile(template, { noEscape: true })(context);
+        return handlebarsCompile(template, { noEscape: true })(context);
       },
       catch: error => (error instanceof Error ? error : new Error(String(error))),
     }),
